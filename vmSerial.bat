ccc2e405b57663b295e3796a5c66f2c2f5c84ab3
HaKDMoDz <HaKDMoDz@live.com>
2015-12-28 01:58:17 -0800

    vmWare Workstation Pro 12 Serial keys Script
    Valid vmWare Workstation Pro 12 Keys valid as of December 2015.

---
+++ vmSerial.bat
@@ -0,0 +1,27 @@
+@echo off
+title VmWare Workstation 12 Pro Keys
+mode 40, 22
+color 4f
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo   VmWare Workstation 12 Pro Keys
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo .
+echo >nul
+echo    AG18A-6HD5P-0811P-ADXNV-Z7KD6
+echo >nul
+echo    ZZ7XU-4QX5Q-M80GP-ZWPZC-P6UVA
+echo >nul 
+echo    CF7DR-F7Y0Q-M8EFZ-RZWQX-QP8Y4
+echo >nul
+echo    CA710-6JZEH-H80GP-Z4W5V-QQHT2
+echo >nul
+echo    GZ5EK-69G03-085EY-PWZNX-YG2V2
+echo >nul
+echo    AZ10A-DXWD5-H89UZ-QMNG9-M2RY8
+echo >nul
+echo    AU3R2-6VX12-H8DVP-K5P5V-QC0GF
+echo >nul
+echo >nul
+echo >nul
+timeout /t 500
+exit
